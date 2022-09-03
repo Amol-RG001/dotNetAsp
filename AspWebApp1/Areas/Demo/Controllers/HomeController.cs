0d0986e83ee2ca0e1d2e77575e7222fa412719e5
amolg10 <amolg10@BSPUNL-023301>
2022-09-03 23:42:51 +0530

    Add project files.

---
+++ AspWebApp1/Areas/Demo/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspWebApp1.Areas.Demo.Controllers
+{
+    [Area("Demo")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("Hello World Bro!");
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+       
+    }
+}
